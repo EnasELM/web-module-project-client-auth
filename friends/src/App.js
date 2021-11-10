aff3f877ec6cc56b7a611d6e54fba6a708110d0c
Enas <enaselmahdwi@gmail.com>
2021-11-09 20:59:48 -0700

    add files

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import './App.css';
+//import { Link } from 'react-router-dom';
+import { Switch, Link } from 'react-router';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import FriendList from './components/FriendList';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import PrivateRoute from './components/PrivateRoute';
+import axiosWithAuth from './utils/axiosWithAuth';
 
 
-const Login = ()=> {
-  return (<h2>Login</h2>)
-}
 
 function App() {
+  const isLoggedIn = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
+  const username = localStorage.getItem('username');
   return (
+    
+    <Router>
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          hhhhhhhhhhhhhhhhhh
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+        <li>
+          {(role === 'admin' && isLoggedIn) && <Link to="/logout">Admin</Link> }
+        </li>
+        <li>
+          {isLoggedIn && <div>
+              <Link to="/protected">Protected Page</Link>
+              <p>Welcome to the page: {username}</p>
+            </div>
+          }
+        </li>
+      </ul>
+      <Switch>
+        <PrivateRoute exact path="/protected" component={FriendList} />
+        {/* <Route exact path="/protected" component={GasPrices} /> */}
+        <Route path="/logout" component={Logout} />
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Login} />    
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import moment from 'moment';
+import Loader from 'react-loader-spinner';
+import axiosWithAuth from './../utils/axiosWithAuth';
+
+class FriendList extends React.Component {
+  state = {
+    friend: []
+  };
+  
+  componentDidMount() {
+    console.log("friend", this.props);
+
+    axiosWithAuth()
+      .get(`/api/friends`)
+      .then(resp=> {
+        this.setState({
+          ...this.state,
+          friend: resp.data.data
+        });
+      })
+      .catch(err=> {
+        console.log(err);
+      })
+  }
+
+  
+
+  render(){ 
+   return(<div>{this.state.friend}</div>) 
+  }
+}
+
+export default FriendList;
